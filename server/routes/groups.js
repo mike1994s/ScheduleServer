b757907e7d8472cf9cb550fb8492eb09db59d617
Mike <meshuin@mail.ru>
2015-04-30 17:23:32 +0400

    plug error,saturday :  number 7

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -20,9 +20,9 @@ exports.get = function(req, res, next) {
             Group.findById(parseUrl.query['group'], function(err, group) {
                 if (err)
                     return next(err);
-                Messages.find({"idGroup": parseUrl.query['group'], "isPublicated": true}, function(err, msgs) {
-                    if (err)
-                        return next(err);
+                Messages.find({"idGroup": parseUrl.query['group'], "isPublicated": true}, function(err1, msgs) {
+                    if (err1)
+                        return next(err1);
                     var arr = [];
                     console.log(arr);
                     res.render('group', {
@@ -34,28 +34,45 @@ exports.get = function(req, res, next) {
             });
         } else {
             var day = parseUrl.query['day'];
-            Group.findById(parseUrl.query['group'], function(err, group) {
-                if (err)
-                    return next(err);
-                Changes.find(function(err, all) {
-                    if (err)
-                        return next(err);
-                    console.log(group["" + day]);
+            Group.findById(parseUrl.query['group'], function(erro, group) {
+                if (erro)
+                    return next(erro);
+                Changes.find(function(err2, all) {
+                    if (err2)
+                        return next(err2);
+//                    console.log(group["" + day]);
                     var dayGroup = [];
                     for (var i = 0; i < group["" + day].length; ++i) {
+                        if (group["" + day][i].number == 7)
+                            continue;
                         if (all.length == 0) {
                             dayGroup.push(group["" + day][i]);
-                            continue;
-                        }
-                        for (var j = 0; j < all.length; ++j) {
-                            if (all[j].idNotice == group["" + day][i]._id) {
-                                if (all[j].day == day)
-                                    dayGroup.push(all[j]);
-                            } else {
-                                dayGroup.push(group["" + day][i]);
+                        } else {
+                            for (var j = 0; j < all.length; ++j) {
+                                if (all[j].idNotice == group["" + day][i]._id) {
+                                    if (all[j].day == day) {
+                                        dayGroup.push(all[j]);
+                                    }
+                                } else {
+                                    var isPresent = false;
+                                    for (var t = 0; t < all.length; ++t) {
+                                        if (all[t].idNotice == group["" + day][i]._id) {
+                                            isPresent = true;
+                                            break;
+                                        }
+                                    }
+                                    if (!isPresent) {
+                                        console.log(group["" + day][i]);
+                                        dayGroup.push(group["" + day][i]);
+                                        break;
+                                    }
+                                }
                             }
                         }
                     }
+//                    for (var h = 0; h < dayGroup.length; ++h) {
+//                        console.log(dayGroup[h]);
+//                    }
                     for (var i = 0; i < all.length; ++i) {
                         if (all[i].day == day && group.name == all[i].groupName) {
                             var isPresent = false;
