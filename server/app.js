de7c76befa637bd4be361daec0babeada1974d0e
mike1994s <meshuin@mail.ru>
2015-05-02 17:18:54 +0400

    Any

--- server/app.js
+++ server/app.js
@@ -9,8 +9,8 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var config = require('config');
-var log = require('libs/log')(module)
-var mongoose = require('libs/mongoose');
+var log = require('./libs/log')(module)
+var mongoose = require('./libs/mongoose');
 var HttpError = require('error').HttpError;
 var app = express();
 app.engine('ejs', require('ejs-locals'));
