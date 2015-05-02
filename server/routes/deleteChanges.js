da7040fe1c3f6854f5e3d87b1742b02f5e7e03c7
mike1994s <meshuin@mail.ru>
2015-05-02 18:15:41 +0400

    a

--- server/routes/deleteChanges.js
+++ server/routes/deleteChanges.js
@@ -1,7 +1,7 @@
 var url = require("url");
-var Group = require('models/group').Group;
-var TeacherLink = require('models/TeacherLink').TeacherLink;
-var Changes = require('models/changes').Changes;
+var Group = require('/models/group').Group;
+var TeacherLink = require('/models/TeacherLink').TeacherLink;
+var Changes = require('/models/changes').Changes;
 var HttpError = require('error').HttpError;
 exports.post = function(req, res, next) {
     console.log(req.body.idDelete);
