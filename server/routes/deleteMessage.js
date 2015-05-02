1344766141e9252dde84e8bd129b64e232adcf95
mike1994s <meshuin@mail.ru>
2015-05-02 18:16:13 +0400

    Update deleteMessage.js

--- server/routes/deleteMessage.js
+++ server/routes/deleteMessage.js
@@ -1,9 +1,9 @@
 var url = require("url");
-var Group = require('models/group').Group;
-var TeacherLink = require('models/TeacherLink').TeacherLink;
-var Changes = require('models/changes').Changes;
-var Messages = require('models/messages').Messages;
-var HttpError = require('error').HttpError;
+var Group = require('/models/group').Group;
+var TeacherLink = require('/models/TeacherLink').TeacherLink;
+var Changes = require('/models/changes').Changes;
+var Messages = require('/models/messages').Messages;
+var HttpError = require('/error').HttpError;
 exports.post = function(req, res, next) {
     console.log(req.body.idDelete);
     if (!req.body.idDelete) {
